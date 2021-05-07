09930fe3543144db77af4add384de6809daf1b63
Brittany Barnett <barnett.brittanym@gmail.com>
2021-05-06 21:35:08 -0400

    Create Polymorphism.py

---
+++ Polymorphism.py
@@ -0,0 +1,44 @@
+#parent class
+class Plants:
+    common_name="Unknown"
+    scientific_name= "Unknown"
+    native_regions = "Unknown"
+    common_uses = "Unknown"
+
+    def information(self):
+        msg= '\nCommon Name: {}\nScientific Name: {}\nNative Regions: {}\nCommon Uses: {}'.format(self.common_name, self.scientific_name, self.native_regions, self.common_uses)
+        return msg
+    
+
+
+#Child Class instance
+class Trees(Plants):
+    common_name='Oak'
+    scientific_name= 'Quercus'
+    native_regions = "North Temperate zone and at high altitude in the tropics"
+    common_uses = "Building and constructing furniture"
+    
+    def facts(self):
+        msg= 'Oak is any of about 450 species of ornamental and timber trees and shrubs constituting the genus Quercus in the beech family. It is a common source of ink and cork production in some countries.'
+        return msg
+    
+#Child class instance
+class Herbs(Plants):
+    common_name='Basil'
+    scientific_name='Oscumum basilicum'
+    native_regions ='Tropical regions from central Africa to Southeast Asia'
+    common_uses = 'used in cuisine worldwide'
+
+    def dishes(self):
+        msg= '\nSauces\nSoups\nPesto\nPizza'
+        return msg
+
+if __name__ == "__main__":
+    a=Trees()
+    b=Herbs()
+    print(a.information())
+    print(a.facts())
+    print(b.information())
+    print(b.dishes())
+    
+    
