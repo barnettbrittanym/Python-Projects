af1a4f78fe878b2358f1b23433eddfcbf87cef44
Brittany Barnett <barnett.brittanym@gmail.com>
2021-05-31 12:48:16 -0400

    Updates made to models.py

--- DjangoUniversity1/Classes/classApp/models.py
+++ DjangoUniversity1/Classes/classApp/models.py
@@ -6,7 +6,10 @@ class djangoClasses(models.Model):
     title = models.CharField(max_length=60)
     CourseNumber = models.IntegerField()
     InstructorName = models.CharField(max_length=60)
-    Duration = models.FloatField()
+    Duration = models.FloatField
+
+    #using the models manager to return the name of the course title
+    objects = models.Manager()
 
     def __str__(self): #changing the name of the instances to the course title name
         return self.title
